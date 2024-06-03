48ab0de5fc73bf51f7af8d75560dbd0be11ccc50
Thomas SOUCHET <thomas.souchet2@etu.univ-nantes.fr>
2024-06-03 11:43:06 +0200

    update(Jeu, Joueur): call API

--- Client/src/main/kotlin/modele/Jeu.kt
+++ Client/src/main/kotlin/modele/Jeu.kt
@@ -1,20 +1,34 @@
 package modele
 
-class Jeu() {
+import kotlinx.coroutines.runBlocking
+
+class Jeu(private val server: Server, firstPlayer: Joueur, private val nbJoueurs: Int) {
+    private val id: Int
+    private var joueurs: Array<Joueur>
+    private val maxPlayers = 8
 
     init {
+        this.joueurs = arrayOf(firstPlayer)
 
+        // Création de la partie avec le serveur
+        runBlocking {
+            this@Jeu.id = this@Jeu.server.createPartie(this@Jeu.joueurs[0].id, this@Jeu.nbJoueurs)
+        }
     }
 
     fun start() {
-
+        TODO()
     }
 
-    fun ajouterJoueur() {
-
-    }
-
-    fun supprimerJoueur() {
-
+    fun ajouterJoueur(j: Joueur): Boolean {
+        return if (this.joueurs.size < this.maxPlayers) {
+            this.joueurs.plus(j)
+            runBlocking {
+                this@Jeu.server.joinPartie(j.id)
+            }
+            true
+        } else {
+            false
+        }
     }
 }
--- Client/src/main/kotlin/modele/Joueur.kt
+++ Client/src/main/kotlin/modele/Joueur.kt
@@ -1,12 +1,22 @@
 package modele
 
-class Joueur(nom: String) {
+import kotlinx.coroutines.runBlocking
+
+class Joueur(nom: String, server: Server) {
     val nom: String
     val currentPlayer: Boolean
+    private val server: Server
+    var id: Int
 
     init {
         this.nom = nom
         this.currentPlayer = false
+        this.server = server
+
+        // Création du joueur avec le serveur
+        runBlocking {
+            this@Joueur.id = this@Joueur.server.createPlayer(this@Joueur.nom)
+        }
     }
 
     fun startGame(): Int {
