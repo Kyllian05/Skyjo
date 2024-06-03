7643948bec52d9ed36caaef290a98f479d7270c8
Kyllian ARNAUD <E236119B@ubuntu.u-inf-j-c005-09>
2024-06-03 15:19:08 +0200

    Main.kt

--- Client/src/main/kotlin/Main.kt
+++ Client/src/main/kotlin/Main.kt
@@ -6,12 +6,14 @@ import vue.Accueil
 class Main: Application() {
 
     override fun start(primaryStage: Stage) {
+
         val Accueil = Accueil()
         val scene = Scene(Accueil, Double.MAX_VALUE, Double.MAX_VALUE)
         scene.stylesheets.add(javaClass.getResource("style.css").toExternalForm())
         primaryStage.title="SKYJO"
         primaryStage.scene=scene
         primaryStage.show()
+        
     }
 }
 fun main(args: Array<String>) {
