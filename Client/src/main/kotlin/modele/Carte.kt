57efc356854c7c42dccada25c388b681f7cb6380
Thomas SOUCHET <thomas.souchet2@etu.univ-nantes.fr>
2024-06-03 10:45:41 +0200

    add javafx to Gradle

--- Client/src/main/kotlin/modele/Carte.kt
+++ Client/src/main/kotlin/modele/Carte.kt
@@ -1,12 +1,20 @@
 package modele
 
+import java.io.FileInputStream
+
+/**
+ * Carte :
+ * @param nombre la valeur de la carte
+ * @property nombre la valeur de la carte
+ * @property file l'image de la carte associée
+ */
 class Carte(nombre: Int) {
+    private val path = "images/cartes/"
     val nombre: Int
-    val couleur: String
+    val file: FileInputStream
 
     init {
         this.nombre = nombre
-        this.couleur = "#000000"
+        this.file = FileInputStream("${this.path}carte${this.nombre}.png")
     }
-
 }
