928e59446dc9162b2a8ccb125c05a22e097a381a
Thomas SOUCHET <thomas.souchet2@etu.univ-nantes.fr>
2024-06-03 13:53:57 +0200

    Début classe Jeu

--- Client/src/main/kotlin/modele/Jeu.kt
+++ Client/src/main/kotlin/modele/Jeu.kt
@@ -2,33 +2,41 @@ package modele
 
 import kotlinx.coroutines.runBlocking
 
-class Jeu(private val server: Server, firstPlayer: Joueur, private val nbJoueurs: Int) {
-    private val id: Int
-    private var joueurs: Array<Joueur>
-    private val maxPlayers = 8
+class Jeu(private val server: Server, player: Joueur) {
+    private var id: Int? = null
+    private val myPlayer: Joueur
+    private var joined: Boolean = false
 
     init {
-        this.joueurs = arrayOf(firstPlayer)
-
-        // Création de la partie avec le serveur
-        runBlocking {
-            this@Jeu.id = this@Jeu.server.createPartie(this@Jeu.joueurs[0].id, this@Jeu.nbJoueurs)
-        }
+        this.myPlayer = player
     }
 
     fun start() {
-        TODO()
+        if (joined) {
+            TODO()
+        } else {
+            throw Exception("No party joined")
+        }
     }
 
-    fun ajouterJoueur(j: Joueur): Boolean {
-        return if (this.joueurs.size < this.maxPlayers) {
-            this.joueurs.plus(j)
-            runBlocking {
-                this@Jeu.server.joinPartie(j.id)
-            }
-            true
-        } else {
-            false
+    fun creerPartie(nbJoueur: Int) {
+        if (joined) {
+            throw Exception("Party already joined")
+        }
+        // Création de la partie avec le serveur
+        runBlocking {
+            this@Jeu.id = this@Jeu.server.createPartie(this@Jeu.myPlayer.id, nbJoueur)
+        }
+        this.joined = true
+    }
+
+    fun rejoindrePartie(idParty: Int): Boolean {
+        if (joined) {
+            throw Exception("Party already joined")
+        }
+        var joueurs = IntArray(8)
+        runBlocking {
+            joueurs = this@Jeu.server.getAllPlayers()
         }
     }
 }
