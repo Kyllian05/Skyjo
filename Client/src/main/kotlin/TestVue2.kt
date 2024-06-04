567ccae0efc0ccafcf4f3ff8bf2337b8c477204f
E239982A <brieuc.le-carluer@etu.univ-nantes.fr>
2024-06-04 14:58:53 +0200

    Créetion Vue Rejoindre

--- Client/src/main/kotlin/TestVue2.kt
+++ Client/src/main/kotlin/TestVue2.kt
@@ -2,6 +2,11 @@ import javafx.application.Application
 import javafx.scene.Scene
 import javafx.stage.Stage
 import vue.CreationPartie
+import modele.Jeu
+import modele.Server
+import vue.Accueil
+import vue.Salon
+import vue.Rejoindre
 
 class TestVue2(): Application() {
 
@@ -10,11 +15,12 @@ class TestVue2(): Application() {
         // Vues
         val vue = CreationPartie()
 
+
         // Scène
         val scene = Scene(vue, 1920.0, 1080.0)
 
         scene.stylesheets.add(javaClass.getResource("style.css").toExternalForm())
-        primaryStage.title="Skyjo"
+        primaryStage.title="Rejoindre"
         primaryStage.scene=scene
         primaryStage.isMaximized = true
         primaryStage.show()
