2e60c8ba9a89795e5d85bf836aa575b4ece6baa7
Pol Lamothe <lamothepol@gmail.com>
2024-06-05 09:48:59 +0200

    Debut controleur rejoindre

---
+++ Client/src/main/kotlin/controleur/rejoindrePartie/ControleurBoutonRejoindrePartie.kt
@@ -0,0 +1,17 @@
+package controleur.rejoindrePartie
+
+import controleur.Salon.FetchingPlayer
+import javafx.event.ActionEvent
+import javafx.event.EventHandler
+import javafx.stage.Stage
+import modele.Jeu
+import vue.Salon
+
+class ControleurBoutonRejoindrePartie(val stage : Stage,val salon : Salon,val jeu : Jeu): EventHandler<ActionEvent> {
+    override fun handle(e: ActionEvent) {
+        //jeu.rejoindrePartie()
+        salon.ListeJoueurs.items = jeu.listeJoueur
+        stage.scene.root = salon
+        FetchingPlayer(jeu,salon).startWaiting()
+    }
+}
