abe977e78913af435c2c2c703bb4b64cb47908f8
Thomas SOUCHET <thomas.souchet2@etu.univ-nantes.fr>
2024-06-11 14:12:58 +0200

    controler: LinkName dans le bon ordre

--- Client/src/main/kotlin/controleur/game/LinkName.kt
+++ Client/src/main/kotlin/controleur/game/LinkName.kt
@@ -5,8 +5,24 @@ import vue.Game
 
 class LinkName(val vue : Game,val jeu:Jeu) {
     init{
-        for (i in 0..<jeu.listeJoueur.size){
-            vue.playersName[i].value = jeu.listeJoueur[i]
+        // Détecter la position du joueur dans la liste
+        var pos = 0
+        for (i in 0 until jeu.listeJoueur.size) {
+            if (jeu.listeJoueur[i] == jeu.myPlayer!!.nom) {
+                pos = i
+                break
+            }
+        }
+        // Placer les noms des joueurs après le nôtre
+        var index = 0
+        for (i in pos+1 until jeu.listeJoueur.size){
+            vue.playersName[index].value = jeu.listeJoueur[i]
+            index++
+        }
+        // Placer les noms des joueurs avant le nôtre
+        for (i in 0 until pos){
+            vue.playersName[index].value = jeu.listeJoueur[i]
+            index++
         }
     }
 }
